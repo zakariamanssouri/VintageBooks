e66d6afbc28c221a8f49f437e37820506282cf8b
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-12-10 00:32:33 +0100

    initial commit

---
+++ lib/data/json.dart
@@ -0,0 +1,98 @@
+var profile = "https://avatars.githubusercontent.com/u/86506519?v=4";
+
+var latestBooks = [
+  {
+    "title": "The Creative Ideas",
+    "image":
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/how-to-creative-ideas-book-cover-design-template-52f7ec58f53452b9b46a351cea1bd9a1_screen.jpg",
+    "price": "\DH 58.99",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "Follow Me to Ground",
+    "image":
+        "https://s26162.pcdn.co/wp-content/uploads/2019/12/46301955-668x1024.jpg",
+    "price": "\DH 19.99",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "Snow at Sunset",
+    "image":
+        "https://wepik.com/storage/previews/1853935/conversions/minimalist-snow-winter-book-cover-r-908311156page1-thumb.jpg",
+    "price": "\DH 29.90",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "The Prince of Thorns",
+    "image":
+        "https://www.thecreativepenn.com/wp-content/uploads/2018/04/image1.jpeg",
+    "price": "\DH 9.99",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "The Last Breath",
+    "image":
+        "https://i.pinimg.com/originals/fc/52/3f/fc523fab7bcc161d8cca966ee974be64.png",
+    "price": "\DH 93.90",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "The Secrets",
+    "image":
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwf6U8c_AwOwZvz9CjfMEzikpESGwcNqSuxQ&usqp=CAU",
+    "price": "\DH 45.50",
+    "ori_price": "\DH 120.50"
+  }
+];
+
+var popularBooks = [
+  {
+    "title": "أنتيخيريستوس",
+    "image":
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/e4da8d22389639.56311e6ede892.jpg",
+    "price": "\DH 80",
+    "ori_price": "\DH 23.50"
+  },
+  {
+    "title": "Thus Spoke Zarathustra",
+    "image":
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqBXbhwP6pXtUxMUqBSog2SBrY4PqnXfEl4HlEUj-yr_XvJIVfN106goAIlufyWLpFi_4&usqp=CAU",
+    "price": "\DH 120",
+    "ori_price": "\DH 23.50"
+  },
+  {
+    "title": "The Power of You",
+    "image":
+        "https://i.pinimg.com/originals/97/c4/99/97c499de2581f8cca7f415b4d85870a5.jpg",
+    "price": "\DH 9.99",
+    "ori_price": "\DH 23.50"
+  },
+  {
+    "title": "The Glow",
+    "image":
+        "https://i.pinimg.com/736x/4f/59/aa/4f59aaa78f898054f949351515875b3c--book-cover-design-book-design.jpg",
+    "price": "\DH 26.50",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "The Happy Morning",
+    "image":
+        "https://pro2-bar-s3-cdn-cf6.myportfolio.com/4573a45834d27f53b119f41019fcc904/a7c97b6f-b81e-4cc5-a59e-c9bdf1b3c746_rw_1200.jpg",
+    "price": "\DH 14.99",
+    "ori_price": "\DH 120.50"
+  },
+  {
+    "title": "Undersea World",
+    "image":
+        "https://www.edrawsoft.com/templates/images/seaworld-children-book-cover.png",
+    "price": "\DH 29.99",
+    "ori_price": "\DH 60.50"
+  },
+  {
+    "title": "The Last Breath",
+    "image":
+        "https://www.ingramspark.com/hubfs/Book-Cover-Design-Pillar/32.png",
+    "price": "\DH 9.99",
+    "ori_price": "\DH 120.50"
+  }
+];
---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primary = Colors.blue;
+const secondary = Colors.white;
+const mainColor = Color(0xFF000000);
+const appBgColor = Color(0xFFF5F5F5);
+const bottomBarColor = Colors.blue;
+const shadowColor = Colors.black87;
